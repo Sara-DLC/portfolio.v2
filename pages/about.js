f1ca5242e7416de96ccf352deb71c43c3419e2aa
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2021-01-31 13:07:25 -0600

    updated about page intro

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 function About() {
   return <div>
     <h1>About Me</h1>
-    <p>Hi! I am Sara De La Cruz a Software Engineer interested in open source and helping non-profits build better software systems to help the community. On my free time I like to watch documentaries, learn a new skill, or simply just chill out and listen to music. If you would like to know anything else please feel free to ask 😁</p>
+    <p>Hi! I am Sara De La Cruz a Software Engineer interested in open source and volunteering my free time to help non-profits build better software systems to help our communities. On my free time I like to watch documentaries, learn a new skill, or simply just laze around and listen to music. If you would like to know anything else please feel free to ask 😁</p>
   </div>
 }
 
