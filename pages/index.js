a55290bb315945b1c2c8f5e90d3938531068ed56
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2021-01-31 13:09:39 -0600

    created message page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import About from './about'
 import Projects from './projects'
+import Message from './message'
 
 function HomePage() {
   return <div>Sara De La Cruz Portfolio
     <About/>
     <Projects/>
+    <Message/>
   </div>
 }
 
